7874af2b5aab4dd467cdac2bb0ac744a7afe30ab
TravisPurcell <travis.n.purcell@gmail.com>
2023-01-16 10:11:24 -0700

    Change className

--- src/modules/Lamp.jsx
+++ src/modules/Lamp.jsx
@@ -10,7 +10,7 @@ function Lamp() {
             <div className="col-12">
               <h2>PHP/SQL Demo</h2>
               <h3>To-Do App</h3>
-              <div className="link__wrapper">
+              <div className={styles.link__wrapper}>
                 <p><a href="https://github.com/TravisPurcell/ToDo/tree/main/themes/todo" target="_blank">Click to View Github Repo</a></p>
               </div>
               <iframe width="100%" src="https://tnp-portfolio.com/php-demo"></iframe>
